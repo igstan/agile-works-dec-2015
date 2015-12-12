c7728819d4a6be0e4803352a808e5d222e09a043
Ionuț G. Stan <ionut.g.stan@gmail.com>
2015-12-12 07:49:02 +0200

    A game state is now a tri-state

--- Main.hs
+++ Main.hs
@@ -9,11 +9,9 @@ data Board = Board [[Mark]]
 data Player = PlayerX | PlayerO
 
 data GameState =
-  GameState {
-    board :: Board,
-    player :: Player,
-    finished :: Bool
-  }
+    GameDraw Board
+  | GameWon Board Player
+  | GameContinue Board Player
 
 type Position = (Int, Int)
 
@@ -31,13 +29,13 @@ readPosition ('A' : n) = readInt n >>= \n -> Right (1, n)
 readPosition ('B' : n) = readInt n >>= \n -> Right (2, n)
 readPosition ('C' : n) = readInt n >>= \n -> Right (3, n)
 readPosition (col : _) = Left ("Invalid column: " ++ [col])
-readPosition pos       = Left ("Invalid position: " ++ pos)
+readPosition position  = Left ("Invalid position: " ++ position)
 
 readInt :: String -> Either String Int
 readInt "1" = Right 1
 readInt "2" = Right 2
 readInt "3" = Right 3
-readInt r   = Left ("Invalid row: " ++ r)
+readInt row = Left ("Invalid row: " ++ row)
 
 getMark :: Player -> Mark
 getMark PlayerX = X
@@ -47,70 +45,88 @@ next :: Player -> Player
 next PlayerX = PlayerO
 next PlayerO = PlayerX
 
+zipWithIndex :: (Enum a, Num a) => (a -> b -> c) -> [b] -> [c]
+zipWithIndex fn xs = zipWith fn [1..] xs
+
 addMark :: Position -> Mark -> Board -> Either String Board
-addMark (col, row) mark (Board rows) = fmap Board $ sequence $ zipWith mapRow [1..] rows
+addMark (col, row) mark (Board rows) = fmap Board . sequence . zipWithIndex mapRow $ rows
   where
-     mapRow i row' = sequence $ zipWith (mapCol i) [1..] row'
-     mapCol i j mark' | i == row && j == col && mark' == E = Right mark
-                      | i == row && j == col && mark' /= E = Left "Position taken. Try again."
-                      | otherwise                          = Right mark'
+    mapRow i row'
+     | i == row  = sequence $ zipWithIndex mapCol row'
+     | otherwise = Right row'
+    mapCol j mark'
+     | j == col && mark' /= E = Left "Position taken. Try again."
+     | j == col && mark' == E = Right mark
+     | otherwise              = Right mark'
 
 hasWinner :: Board -> Mark -> Bool
 hasWinner (Board rows) mark =
-  or [
-    checkRows rows,
-    checkRows . transpose $ rows,
-    checkRows [diagonal rows [0,1,2]],
-    checkRows [diagonal rows [2,1,0]]
+  any ($ rows) [
+    any checkRow,
+    any checkRow . transpose,
+    checkRow . diagonal [0,1,2],
+    checkRow . diagonal [2,1,0]
   ]
   where
-    checkRows = any $ all (== mark)
-    diagonal = zipWith (!!)
+    checkRow = all (== mark)
+    diagonal = flip $ zipWith (!!)
 
 isFull :: Board -> Bool
 isFull (Board rows) = all (/= E) $ concat rows
 
 emptyBoard = Board [[E,E,E], [E,E,E], [E,E,E]]
-initialState = GameState emptyBoard PlayerX False
+initialState = GameContinue emptyBoard PlayerX
 
-nextState' :: String -> GameState -> Either String (String, Bool, Board)
-nextState' line gameState =
+placeMark :: String -> GameState -> Either String GameState
+placeMark line (GameContinue board player) =
   do
-    pos <- readPosition line
-    newBoard <- addMark pos (getMark $ player gameState) (board gameState)
-    let (msg, finished) = if hasWinner newBoard X || hasWinner newBoard O
-        then (announceVictor $ player gameState, True)
-        else
-          if isFull newBoard
-          then (announceDraw, True)
-          else (show newBoard, False)
-    return (msg, finished, newBoard)
+    position <- readPosition line
+    newBoard <- addMark position (getMark player) board
+    return $ decideWinner newBoard
   where
-    announceVictor PlayerX = "X has won"
-    announceVictor PlayerO = "O has won"
-    announceDraw = "It's a draw!"
+    decideWinner board =
+      if hasWinner board X || hasWinner board O
+      then GameWon board player
+      else
+        if isFull board
+        then GameDraw board
+        else GameContinue board (next player)
+
+announceVictor PlayerX = "X has won"
+announceVictor PlayerO = "O has won"
+announceDraw = "It's a draw!"
+
+showState :: GameState -> IO ()
+showState gameState = do
+  case gameState of
+    GameContinue board player -> putStrLn (show board)
+    GameWon board player      -> putStrLn (show board) >> putStrLn (announceVictor player)
+    GameDraw board            -> putStrLn (show board) >> putStrLn announceDraw
+
+readLine :: String -> IO String
+readLine prompt = putStr prompt >> getLine
 
 nextState :: GameState -> IO GameState
-nextState gameState@(GameState board player finished) =
-  do
-    line <- getLine
-    result <- return $ nextState' line gameState
-    let (msg, finished, newBoard, newPlayer) =
-          case result of
-            Left msg -> (msg, False, board, player)
-            Right (msg, finished, newBoard) -> (msg, finished, newBoard, next player)
-    putStrLn msg
-    return $ GameState newBoard newPlayer finished
+nextState gameState @ (GameContinue board player) = do
+  putStrLn "———————————————————————————————————————————————————————————————————"
+  line <- readLine "position: "
+  putStrLn "———————————————————————————————————————————————————————————————————"
+  case placeMark line gameState of
+    Left msg        -> putStrLn msg >> return (GameContinue board player)
+    Right gameState -> showState gameState >> return gameState
 
 iterateUntilM :: (Monad m) => (a -> Bool) -> (a -> m a) -> a -> m a
 iterateUntilM p f v
-    | p v       = return v
-    | otherwise = f v >>= iterateUntilM p f
+  | p v       = return v
+  | otherwise = f v >>= iterateUntilM p f
+
+hasFinished :: GameState -> Bool
+hasFinished (GameDraw _)       = True
+hasFinished (GameWon _ _)      = True
+hasFinished (GameContinue _ _) = False
 
 main :: IO GameState
-main =
-  do
+main = do
   putStrLn "Hello!"
   putStrLn "Let's play Tic-Tac-Toe!"
-  iterateUntilM finished nextState initialState
-
+  iterateUntilM hasFinished nextState initialState
